973b2484865df6fd9d31bb12441b1332b0dd72b5
bretttullypersonal <brett.tully@gmail.com>
2015-06-11 14:14:25 +0100

    Added a function to python wrapping to prevent new attributes to be added to wrapped classes

--- example/simple.py
+++ example/simple.py
@@ -29,7 +29,7 @@ def run(run_pure_python=False, run_profiling=False):
     grid_spacing = 251
     secs_in_day = 86400.0
     initial_time = 0.0
-    num_steps = 8640
+    num_steps = 5
     dt = secs_in_day / num_steps
     write_transient = False
     write_wall = True
@@ -82,6 +82,7 @@ def run(run_pure_python=False, run_profiling=False):
         s = CPPSolver(grid_spacing, initial_time, num_steps, dt,
                       write_transient, write_wall, debug_print, base_name, opts)
     result = s.solve()
+    print "{}: {}".format(base_name, result)
 
     if run_profiling:
         mem_profiling_on = profiling.profiling_memory_on()
@@ -113,31 +114,6 @@ def run(run_pure_python=False, run_profiling=False):
                 except subprocess.CalledProcessError as err:
                     print err
 
-    output1 = list()
-    output1.append(base_name)
-    output1.append(opts.alpha_a)
-    output1.append(opts.beta_a)
-    output1.append(opts.kappa_a)
-    output1.append(opts.alpha_c)
-    output1.append(opts.beta_c)
-    output1.append(opts.kappa_c)
-    output1.append(opts.k_ce)
-    output1.append(opts.alpha_v)
-    output1.append(opts.beta_v)
-    output1.append(opts.kappa_v)
-    output1.append(opts.gamma_ac)
-    output1.append(opts.gamma_ce)
-    output1.append(opts.gamma_cv)
-    output1.append(opts.gamma_ev)
-
-    output2 = list()
-    output2.append(result.displacement)
-    output2.append(result.pressure_art)
-    output2.append(result.pressure_cap)
-    output2.append(result.pressure_csf)
-    output2.append(result.pressure_ven)
-    return [output1, output2]
-
 
 if __name__ == "__main__":
     # import timeit
@@ -146,6 +122,7 @@ if __name__ == "__main__":
     # print "C++:"
     # print timeit.timeit("from simple import run; run(run_pure_python=False)", number=10)
 
+    # run(run_pure_python=True, run_profiling=False)
     # run(run_pure_python=True, run_profiling=True)
 
     import matplotlib.pyplot as plt
@@ -153,14 +130,10 @@ if __name__ == "__main__":
     if True:
         import multiprocessing as mp
         pool = mp.Pool(processes=mp.cpu_count())
-        results = pool.map(run, [True, False])
+        pool.map(run, [True, False])
     else:
-        results = list()
-        results.append(run(run_pure_python=True))
-        results.append(run(run_pure_python=False))
-
-    for result in results:
-        print result
+        run(run_pure_python=True)
+        run(run_pure_python=False)
 
     p = np.genfromtxt("example_python_wall.dat", skiprows=1, delimiter=", ")
     c = np.genfromtxt("example_cpp_wall.dat", skiprows=1, delimiter=", ")
